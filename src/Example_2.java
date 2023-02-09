1d6dafc011323b6deed920ea514852ff02efeb19
meera <meera@192.168.10.140>
2023-02-09 14:42:00 -0600

    I have completed task

---
+++ src/Example_2.java
@@ -0,0 +1,16 @@
+
+public class Example_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("wap for addition of two numbers");
+		int a=10, b=20, c;
+		c =  a+b;
+		System.out.println("addition of two numbers:"+c);
+		System.out.println("Statement end of Example-02");
+		
+
+	}
+
+}
